c231afa6b2668cea7f5f1d253e08ce7d1cfbbc76
Veronika <shagiyanovavn@gmail.ru>
2023-12-12 17:22:58 +0300

    edit test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,31 +3,31 @@ from src import utils
 
 
 def test_distribation_by_dates():
-    assert utils.distribation_by_dates([{"state": "EXECUTED",
-    "date": "2019-08-26T10:50:58.294041"}, {},
-    { "state": "EXECUTED",
-    "date": "2018-12-20T16:43:26.929246",},
-    {"state": "CANCELED",
-    "date": "2018-09-12T21:27:25.241689"}]) == [{"state": "EXECUTED",
-    "date": "2019-08-26T10:50:58.294041"}, { "state": "EXECUTED",
-    "date": "2018-12-20T16:43:26.929246"}]
+    assert (utils.distribation_by_dates([{"state": "EXECUTED",
+                                         "date": "2019-08-26T10:50:58.294041"}, {},
+                                        {"state": "EXECUTED",
+                                         "date": "2018-12-20T16:43:26.929246"},
+                                        {"state": "CANCELED",
+                                         "date": "2018-09-12T21:27:25.241689"}]) ==
+            [{"state": "EXECUTED", "date": "2019-08-26T10:50:58.294041"},
+             {"state": "EXECUTED", "date": "2018-12-20T16:43:26.929246"}])
+
 
 @pytest.fixture
 def item():
     return [{
-    "id": 441945886,
-    "state": "EXECUTED",
-    "date": "2019-08-26T10:50:58.294041",
-    "operationAmount": {
-      "amount": "31957.58",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "Maestro 1596837868705199",
-    "to": "Счет 64686473678894779589"
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {"amount": "31957.58",
+                            "currency": {
+                                            "name": "руб.",
+                                            "code": "RUB"
+                                        }
+                            },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
     },
         {
             "id": 587085106,
@@ -43,6 +43,8 @@ def item():
             "description": "Открытие вклада",
             "to": "Счет 41421565395219882431"
         }]
+
+
 def test_date_operation(item):
     assert utils.date_operation(item[0]) == '26.08.2019'
     assert utils.date_operation(item[1]) == '23.03.2018'
